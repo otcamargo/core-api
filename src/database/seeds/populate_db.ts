362da997d4fb9515d6e6ec8046b0bf346647e579
Otavio Camargo <otavioacamargo@outlook.com>
2021-02-11 23:41:54 -0300

    Fix seeds

--- src/database/seeds/populate_db.ts
+++ src/database/seeds/populate_db.ts
@@ -2,5 +2,6 @@ import Knex from "knex";
 
 export async function seed(knex: Knex) {
   await knex('todo_items').insert([
-    { user_id: '1', content: 'teste', status: 0 },
+    { user_id: '1', content: 'teste', status: 0 }
+  ]);
 }
