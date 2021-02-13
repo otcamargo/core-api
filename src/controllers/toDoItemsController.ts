0e6cdbf25fe16cb72fdfb66da34b378e554ae86e
Otavio Camargo <otavioacamargo@outlook.com>
2021-02-12 23:13:49 -0300

    Build post endpoint

--- src/controllers/toDoItemsController.ts
+++ src/controllers/toDoItemsController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class ToDoItemsController {
-  async index (request: Request, response: Response) {
+  async index(request: Request, response: Response) {
     const toDoItems = await knex('todo_items').select('*');
 
     const serializedToDoItems = toDoItems.map(item => {
@@ -15,6 +15,30 @@ class ToDoItemsController {
     });
 
     return response.json(serializedToDoItems);
+  }
+
+  async create(request: Request, response: Response) {
+    const {
+      user_id,
+      content,
+      status
+    } = request.body
+
+    const trx = await knex.transaction();
+
+    const toDoItem = {
+      user_id,
+      content,
+      status
+    }
+
+    console.log(toDoItem);
+
+    const insertedItem = await trx('todo_items').insert(toDoItem);
+
+    await trx.commit();
+
+    return response.json({ toDoItem });
   }
 }
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,5 +5,6 @@ const routes = express.Router();
 const toDoItemsController = new ToDoItemsController();
 
 routes.get('/todo_items', toDoItemsController.index);
+routes.post('/todo_items', toDoItemsController.create);
 
 export default routes;
