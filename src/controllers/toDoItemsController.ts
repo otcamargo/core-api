ff5038d40614c40efd77e13b49d3b6465fcbaa4e
Otávio Camargo <45431946+otcamargo@users.noreply.github.com>
2021-02-16 02:01:01 -0300

    Extract logic to models (#5)
    * Remove console log
    
    * Begin extracting business logic to models
    
    * Extract business logic to model class

--- src/controllers/toDoItemsController.ts
+++ src/controllers/toDoItemsController.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from 'express';
 import knex from '../database/connection';
+import ToDoItem from '../models/ToDoItem';
 
 class ToDoItemsController {
   async index(request: Request, response: Response) {
-    const toDoItems = await knex('todo_items').select('*');
+    const toDoItemModel = new ToDoItem();
+    const allItems = await toDoItemModel.all();
 
-    const serializedToDoItems = toDoItems.map(item => {
+    const serializedToDoItems = allItems.map(item => {
       return {
         id: item.id,
         user_id: item.user_id,
@@ -24,21 +26,10 @@ class ToDoItemsController {
       status
     } = request.body
 
-    const trx = await knex.transaction();
+    const toDoItemModel = new ToDoItem();
+    const created_item = await toDoItemModel.create(user_id, content, status);
 
-    const toDoItem = {
-      user_id,
-      content,
-      status
-    }
-
-    console.log(toDoItem);
-
-    const insertedItem = await trx('todo_items').insert(toDoItem);
-
-    await trx.commit();
-
-    return response.json({ toDoItem });
+    return response.json(created_item);
   }
 
   async update(request: Request, response: Response) {
@@ -48,14 +39,11 @@ class ToDoItemsController {
       status
     } = request.body
 
-    const updatedItem = await knex('todo_items').where('id', id).first().update({
-      content: content,
-      status: status
-    });
+    const toDoItemModel = new ToDoItem();
+    const updatedItem = await toDoItemModel.update(id, content, status);
 
-    if(updatedItem) {
-      const item = await knex('todo_items').where('id', id).first();
-      return response.json(item);
+    if (updatedItem) {
+      return response.json(updatedItem);
     } else {
       return response.status(404).send({ error: response.statusCode });
     }
@@ -64,11 +52,11 @@ class ToDoItemsController {
   async delete(request: Request, response: Response) {
     const { id } = request.params;
 
-    const item = await knex('todo_items').where('id', id).first();
+    const toDoItemModel = new ToDoItem();
+    const deletedItem = await toDoItemModel.delete(id);
 
-    if(item) {
-      await knex('todo_items').where('id', id).first().delete();
-      return response.json(item);
+    if(deletedItem) {
+      return response.json(deletedItem);
     } else {
       return response.status(404).send({ error: response.statusCode });
     }
