f00958bf92e8b105b1fdbdb4f49911d4b67b8990
Otavio Camargo <otavioacamargo@outlook.com>
2021-02-21 20:46:30 -0300

    Add authentication to the project

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import ToDoItemsController from '../controllers/toDoItemsController';
+import auth from "./auth";
+import user from "./user";
 
 const routes = express.Router();
 const toDoItemsController = new ToDoItemsController();
@@ -9,4 +11,11 @@ routes.post('/todo_items', toDoItemsController.create);
 routes.delete('/todo_items/:id', toDoItemsController.delete);
 routes.patch('/todo_items/:id', toDoItemsController.update);
 
+routes.use("/auth", auth);
+routes.use("/user", user);
+
 export default routes;
+
+
+
+//https://js.plainenglish.io/creating-a-rest-api-with-jwt-authentication-and-role-based-authorization-using-typescript-fbfa3cab22a4
