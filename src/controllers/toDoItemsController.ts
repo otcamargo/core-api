90e82ee75571ae7015f1d70efbb1b5e4f11a4664
Otavio Camargo <otavioacamargo@outlook.com>
2021-02-22 00:38:16 -0300

    Use authentication in other endpoints

--- src/controllers/toDoItemsController.ts
+++ src/controllers/toDoItemsController.ts
@@ -4,7 +4,8 @@ import { TodoItem } from '../entity/TodoItem';
 
 class ToDoItemsController {
   async index(request: Request, response: Response) {
-    const allItems = await getRepository(TodoItem).find({ order: { id: 'ASC'} })
+    const userId = response.locals.jwtPayload.userId;
+    const allItems = await getRepository(TodoItem).find({ where: { user_id: userId }, order: { id: 'ASC' } })
 
     const serializedToDoItems = allItems.map(item => {
       return {
@@ -18,37 +19,54 @@ class ToDoItemsController {
   }
 
   async create(request: Request, response: Response) {
-    const createdItem = await getRepository(TodoItem).save(request.body);
+    const userId = response.locals.jwtPayload.userId;
+
+    const newItem = {
+      user_id: userId,
+      content: request.body.content
+    }
+
+    const createdItem = await getRepository(TodoItem).save(newItem);
     return response.json(createdItem);
   }
 
   async update(request: Request, response: Response) {
     const { id } = request.params;
+    const userId = response.locals.jwtPayload.userId;
     const {
       content,
       status
     } = request.body
 
     const updatedItem = {
+      user_id: userId,
       content: content,
       status: status
     }
 
-    const outdatedItem = await getRepository(TodoItem).findOneOrFail(id);
-    outdatedItem.content = updatedItem.content;
-    outdatedItem.status = updatedItem.status;
-    await getRepository(TodoItem).save(outdatedItem);
 
-    return response.json(updatedItem);
+    try {
+      const outdatedItem = await getRepository(TodoItem).findOneOrFail(id);
+      outdatedItem.content = updatedItem.content;
+      outdatedItem.status = updatedItem.status;
+      await getRepository(TodoItem).save(outdatedItem);
+      return response.json(updatedItem);
+    } catch (error) {
+      response.status(404).send("Not found");
+    }
   }
 
   async delete(request: Request, response: Response) {
     const { id } = request.params;
+    const userId = response.locals.jwtPayload.userId;
     
-    const deletedItem = await getRepository(TodoItem).findOne(id);
-    await getRepository(TodoItem).delete(id);
-    
-    return response.json(deletedItem);
+    try {
+      const deletedItem = await getRepository(TodoItem).findOneOrFail({ where: { user_id: userId, id: id }});
+      await getRepository(TodoItem).delete(id);
+      return response.json(deletedItem);
+    } catch (error) {
+      response.status(404).send("Not found");
+    }
   }
 }
 
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import ToDoItemsController from '../controllers/toDoItemsController';
+import { checkJwt } from '../middlewares/checkJwt';
+import { checkRole } from '../middlewares/checkRole';
 import auth from "./auth";
 import user from "./user";
 
 const routes = express.Router();
 const toDoItemsController = new ToDoItemsController();
 
-routes.get('/todo_items', toDoItemsController.index);
-routes.post('/todo_items', toDoItemsController.create);
-routes.delete('/todo_items/:id', toDoItemsController.delete);
-routes.patch('/todo_items/:id', toDoItemsController.update);
+routes.get('/todo_items', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.index);
+routes.post('/todo_items', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.create);
+routes.delete('/todo_items/:id', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.delete);
+routes.patch('/todo_items/:id', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.update);
 
 routes.use("/auth", auth);
 routes.use("/user", user);
