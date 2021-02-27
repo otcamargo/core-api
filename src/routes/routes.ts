35a41388ecd93c80a5505872cd8b79dc705d0dc6
Otávio Camargo <45431946+otcamargo@users.noreply.github.com>
2021-02-27 20:10:45 -0300

    Refactor routes (#12)
    * Refactor routes
    
    * Add blank line

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,19 +1,12 @@
 import express from 'express';
-import ToDoItemsController from '../controllers/toDoItemsController';
-import { checkJwt } from '../middlewares/checkJwt';
-import { checkRole } from '../middlewares/checkRole';
-import auth from "./auth";
-import user from "./user";
+import auth from './auth';
+import user from './user';
+import todoItem from './todoItem';
 
 const routes = express.Router();
-const toDoItemsController = new ToDoItemsController();
 
-routes.get('/todo_items', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.index);
-routes.post('/todo_items', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.create);
-routes.delete('/todo_items/:id', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.delete);
-routes.patch('/todo_items/:id', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.update);
-
-routes.use("/auth", auth);
-routes.use("/user", user);
+routes.use('/auth', auth);
+routes.use('/user', user);
+routes.use('/todo_item', todoItem);
 
 export default routes;
