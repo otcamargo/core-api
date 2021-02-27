8a3d99a758ccbde7f8382f0e27d7320234b4aa9a
Otavio Camargo <otavioacamargo@outlook.com>
2021-02-27 20:07:08 -0300

    Refactor routes

---
+++ src/routes/todoItem.ts
@@ -0,0 +1,14 @@
+import ToDoItemsController from '../controllers/toDoItemsController';
+import { checkJwt } from '../middlewares/checkJwt';
+import { checkRole } from '../middlewares/checkRole';
+import router from './auth';
+
+const toDoItemsController = new ToDoItemsController();
+
+router.get('/', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.index);
+router.post('/', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.create);
+router.delete('/:id', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.delete);
+router.patch('/:id', [checkJwt, checkRole(["ADMIN"])], toDoItemsController.update);
+
+
+export default router;
